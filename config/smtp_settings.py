8ec53d8718079c75938759412040b10996c74ef2
Anthony Sung <sungpinyue@gmail.com>
2026-01-26 00:31:11 +0800

    fix: smtp setting

--- config/smtp_settings.py
+++ config/smtp_settings.py
@@ -4,7 +4,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 465
 
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "sungpinyue@gmail.com")
 
