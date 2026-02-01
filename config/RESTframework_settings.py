bea642c38f6ab8a8d991b24593ed9ed9eb8e30a9
Anthony Sung <sungpinyue@gmail.com>
2026-02-01 16:12:17 +0800

    fix: prolong JWT life

--- config/RESTframework_settings.py
+++ config/RESTframework_settings.py
@@ -22,10 +22,10 @@ SPECTACULAR_SETTINGS = {
 
 # Simple JWT 設定
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "ROTATE_REFRESH_TOKENS": False,
-    "BLACKLIST_AFTER_ROTATION": False,
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": True,
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
